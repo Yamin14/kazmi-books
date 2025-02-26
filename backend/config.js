428af2b96a73c7207fab9fb9fdfed283797f1639
Yamin14 <syalikazmi@gmail.com>
2025-02-26 04:49:34 -0800

    first commit

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    port: process.env.PORT || 3000,
-    mongoUrl: process.env.mongoUrl || 'mongodb+srv://syalikazmi:6Cjy4ia3qwjs8Fk@books.tbigg.mongodb.net/?retryWrites=true&w=majority&appName=Books'
+    port: process.env.PORT,
+    mongoUrl: process.env.mongoUrl
 }
 
 export default config;
